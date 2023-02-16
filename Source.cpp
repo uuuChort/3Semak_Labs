02a32a7caaa899e8edc9aad016f47aaa7d76fbc9
uuuChort <93771103+uuuChort@users.noreply.github.com>
2023-02-16 15:21:27 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	unsigned long long int k;
+	cin >> k;
+	unsigned long long int g = k, a = 1, b = 1, c = 1;
+	//ћаксимальное степень числа
+	while (g >= 3) {
+		a = a + 1;
+		g = g / 3;
+	}
+	g = k;
+	while (g >= 5) {
+		b = b + 1;
+		g = g / 5;
+	}
+	g = k;
+	while (g >= 7) {
+		c = c + 1;
+		g = g / 7;
+	}
+	
+	//перебор
+	double ss;
+	for (int i = 0; i <= a; i++) {
+		for (int j = 0; j <= b; j++) {
+			for (int p = 0; p <= c; p++) {
+				ss = pow(3, i) * pow(5, j) * pow(7, p);
+				if (ss <= k) {
+					std::cout << ss << "; ";
+				}
+			}
+		}
+	}
+}
+
+
+
