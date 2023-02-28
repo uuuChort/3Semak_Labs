4511a34a72d0a3ddbe10e6bc1fd77c90e1739282
uuuChort <93771103+uuuChort@users.noreply.github.com>
2023-02-28 14:34:09 +0200

    Update Laba_13.py

--- Laba_13.py
+++ Laba_13.py
@@ -1,3 +1,7 @@
+# Дан текстовый файл с текстом на русском или английском произв. длины (организовать чтение).
+# выбрав некоторую хеш-функцию, создать хеш-таблицу с наложением.
+
+#для хеширование
 def HashFunction(s):
     result = 0
     for i in range(len(s)):
@@ -5,31 +9,74 @@ def HashFunction(s):
     result = result % 32
     return result
 
+# вставка
 def insert(mas, line):
     key = HashFunction(line)
+    # если не пустой
     if mas[key] != []:
         first = line
         second = mas[key]
         flag = True
         i = 0
         while (first[i] == second[i]):
-            i += 1
+            i+=1
         else:
             flag = False
         if flag:
             return "This row is already in the table"
         else:
-            mas[key].append(line)
+            k = 0
+            while mas[key+k] != []:
+                k+=1
+            else:
+                mas[key+k] = line
     else:
-        mas[key].append(line)
+        mas[key] = line
 
 
-mas = [[] for i in range(32)]
-file = open("input.txt", 'r')
-out = open("output.txt", 'w')
+#удаление
+def delete(mas, line):
+    key = HashFunction(line)
+    k = 0
+    while mas[key+k]!=line:
+        k+=1
+    else:
+        del mas[key]
+
+# поиск в таблице
+def find(mas, line):
+    key = HashFunction(line)
+    if mas[key] != []:
+        k = key
+        while k < len(mas):
+            if mas[k] == line:
+                return mas[k]
+            else:
+                k+=1
+    else:
+        return "None"
+
+mas = [[] for i in range(60)]
+file = open("input", 'r')
 for line in file:
     line = ' '.join(line.split())
     insert(mas, line)
 for i in range(len(mas)):
     print(i, mas[i])
-    out.write(str(i)+str(mas[i])+'\n')
+
+#тест
+if __name__ == "__main__":
+    mas = [[] for i in range(32)]
+    file = open("input", 'r')
+    for line in file:
+        line = ' '.join(line.split())
+        insert(mas, line)
+    for i in range(len(mas)):
+        print(i, mas[i])
+    a = find(mas, "We all like different genres, but we never argue with each other.")
+    print(a)
+    b = find(mas, "We begin to prepare in advance")
+    delete(mas, "We all like different genres, but we never argue with each other.")
+    a = find(mas, "We all like different genres, but we never argue with each other.")
+    print(a)
+    print(b)
